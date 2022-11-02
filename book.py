4a1d6f7ea98d87a348e9fa9dc4c9dbb141d77933
Arjun R Warrier <notwarrier@gmail.com>
2022-11-02 12:14:47 +0530

    Added dbconnection, added bookadd option

--- book.py
+++ book.py
@@ -1,3 +1,9 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host = 'localhost', user = 'root', password = '', database = 'librarydb')
+
+mycursor = mydb.cursor()
+
 while True:
     print("Select an option from the menu ")
     print("1. Add a book")
@@ -10,6 +16,18 @@ while True:
     choice = int(input("Enter an option: "))
     if(choice == 1):
         print("book enter selected")
+        bname = input("Enter book name: ")
+        bauthor = input("Enter author: ")
+        blanguage = input("Enter the book language: ")
+        bcategory = input("Enter the book category: ")
+        bcharge = input("Enter the bookcharge/day: ")
+         
+
+        sql = 'INSERT INTO `books`(`bookname`, `author`, `language`, `category`, `charge/day`) VALUES (%s,%s,%s,%s,%s)'
+        data = (bname,bauthor,blanguage,bcategory,bcharge)
+        mycursor.execute(sql,data)
+        mydb.commit()
+        print("book data inserted successfully.")
         
     elif(choice == 2):
         print("View book")
